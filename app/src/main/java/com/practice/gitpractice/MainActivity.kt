fb866a8927af7a56aa8c27f00a7526550e1dc49a
BrandonQuintanilla <brandonquintanillaq@gmail.com>
2022-01-06 13:57:25 -0500

    commit en new-branch-name

--- app/src/main/java/com/practice/gitpractice/MainActivity.kt
+++ app/src/main/java/com/practice/gitpractice/MainActivity.kt
@@ -14,3 +14,19 @@ class MainActivity : AppCompatActivity() {
 
     }
 }
+/**
+ * git branch new-branch-name ->  Crea un nuevo branch desde el nodo en el que me encuentro
+ *
+ * git branch new-branch-name commit-id -> Crea un branch desde el nodo con el ID especificado (revitionID-8digits)
+ *
+ * Solo se pueden crear braches en el repositorio local,
+ * Crear un branch en el repositorio remoto implica PUBLICAR un branch que se encuentra en local
+ *
+ * git log -> muestra el listado de commits recientes
+ * */
+
+/**
+ * git checkout other-branch-name
+ * git switch other-branch-name
+ * -> son comandos equivalentes , switch tiene por unica finalidad el cambiar de branch, checkout tiene muchos mas usos
+ * **/
