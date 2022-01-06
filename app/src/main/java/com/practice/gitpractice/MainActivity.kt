31b18770e3048d015ed56a46eb48f47da0c15788
BrandonQuintanilla <brandonquintanillaq@gmail.com>
2022-01-06 14:01:06 -0500

    Aviso para irse a new-branch-name

--- app/src/main/java/com/practice/gitpractice/MainActivity.kt
+++ app/src/main/java/com/practice/gitpractice/MainActivity.kt
@@ -5,12 +5,12 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Los comentarios de teoria de git se encuentra en new-branch-name
+     * */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //      setContentView(R.layout.activity_main)
-
-
-
-
     }
 }
