5491d328a9ccdaf44bda09034e9c59b112f7dd43
brandon <bquintanilla@urbano.com.pe>
2023-01-05 21:28:08 -0500

    ADD cherry-pick command comments

--- app/src/main/java/com/practice/gitpractice/MainActivity.kt
+++ app/src/main/java/com/practice/gitpractice/MainActivity.kt
@@ -15,8 +15,10 @@ class MainActivity : AppCompatActivity() {
         //Remember that this works by creating a new commit instance
 // git cherry-pick <commit-id> : copies the selected commit over our current branch  (on HEAD)
 // git cherry-pick <commit-id1> <commit-id1> <commit-id2> : be can cherypick many commits, but be carefull with the order. The commit-id1  should be the older than id2
-// git cherry-pick <commit-id> -n : -n arguments tells git that not commit the changes, just apply the commit over our current code
+//Example:         git cherry-pick a7d103b1 04702eed cab51785
 
+// git cherry-pick <commit-id> -n : -n arguments tells git that not commit the changes, just apply the commit over our current code
+//OBS if there are conflic that are too hard to resolve you can use "git cherry-pick --abort" to come back as when the code was before you used the cherry-pick command.
 
         // remember, when we  want to delete a commit  we use
         // git reset --hard HEAD~1 : the ~1 says "how many commits yo want o delete back from current HEAD"
